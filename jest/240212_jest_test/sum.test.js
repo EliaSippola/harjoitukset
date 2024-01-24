b3498503c8a4cf028fb4346c121ede17e69fdc34
Elia Sippola <elia.sippola@edu.riveria.fi>
2024-01-24 11:12:16 +0200

    added gitignore

--- jest/240212_jest_test/sum.test.js
+++ jest/240212_jest_test/sum.test.js
@@ -4,6 +4,15 @@ test('1 + 2 = 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
+test('-1 + 2 = 1', () => {
+    expect(sum(-1, 2)).toBe(1);
+    expect(sum(1, -2)).toBe(-1);
+});
+
+test('-1 + (-2) = -3', () => {
+    expect(sum(-1, -2)).toBe(-3);
+});
+
 test('1 + 2 != 4', () => {
     expect(sum(1, 2)).not.toBe(4);
 });
