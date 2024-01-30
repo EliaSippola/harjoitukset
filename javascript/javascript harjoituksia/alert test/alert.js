2c1f02d6fb54f4c390a10373efe84858e46945c6
Elia Sippola <elia.sippola@edu.riveria.fi>
2024-01-30 14:05:30 +0200

    240130 loppu

---
+++ javascript/javascript harjoituksia/alert test/alert.js
@@ -0,0 +1,26 @@
+function openAlert(alertText) {
+    const backround = document.createElement("div");
+    //const container = document.createElement("div");
+    const alert = document.createElement("div");
+    const text = document.createElement("p");
+    const button = document.createElement("button");
+
+    backround.appendChild(alert);
+    alert.appendChild(text);
+    alert.appendChild(button);
+
+    backround.classList.add("alert-backround");
+    alert.classList.add("alert-box");
+    text.classList.add("alert-text");
+    button.classList.add("alert-button");
+
+    text.innerText = alertText;
+
+    document.body.appendChild(backround);
+
+    document.querySelector(".alert-button").addEventListener("click", closeAlert);
+}
+
+function closeAlert() {
+    document.querySelector(".alert-backround").remove();
+}
